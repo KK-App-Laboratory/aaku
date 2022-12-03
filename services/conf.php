3d5f1985a7a532dbf00b3df8b61d205b3912ce00
@Kumar <krishnakumar.1905059@srec.ac.in>
2022-12-03 19:33:04 +0530

    Update conf.php

--- services/conf.php
+++ services/conf.php
@@ -6,9 +6,9 @@ class CONF {
     public $DEMO_VERSION = false;
 
     /* Data configuration for database */
-    public $DB_SERVER   = "localhost";
+    public $DB_SERVER   = "http://172.31.28.44";
     public $DB_USER     = "root";
-    public $DB_PASSWORD = "";
+    public $DB_PASSWORD = "Aakash7152";
     public $DB_NAME     = "etron_diy";
 
     /* [ IMPORTANT ] be careful when edit this security code, use AlphaNumeric only*/
