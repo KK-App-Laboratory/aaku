a084382b6d760a5ed91f77304be906074506df77
RANJAN KUMAR SAH <60313003+AakashRanjansh@users.noreply.github.com>
2022-12-04 12:27:03 +0530

    Update conf.php

--- services/conf.php
+++ services/conf.php
@@ -8,7 +8,7 @@ class CONF {
     /* Data configuration for database */
     public $DB_SERVER   = "localhost";
     public $DB_USER     = "root";
-    public $DB_PASSWORD = "";
+    public $DB_PASSWORD = "Aakash7152";
     public $DB_NAME     = "etron_diy";
 
     /* [ IMPORTANT ] be careful when edit this security code, use AlphaNumeric only*/
